7a3a23ca67afeea6f2951f82c105eb13ebdace8b
Thomas De Grava <thomas_alexandre96@hotmail.com>
2025-09-12 11:00:43 +0100

    add year calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,9 +1,42 @@
+const months = [
+  "janeiro",
+  "fevereiro",
+  "março",
+  "abril",
+  "maio",
+  "junho",
+  "julho",
+  "agosto",
+  "setembro",
+  "outubro",
+  "novembro",
+  "dezembro",
+];
+
 function Calendar() {
   return (
-    <div>
-      <h2>Calendar</h2>
+    <div className="grid grid-cols-4">
+      {months.map((name, idx) => (
+        <div
+          key={idx}
+          className="rounded-2xl shadow-lg border border-slate-200 bg-transparent flex items-stretch"
+        >
+          <div className="w-full p-4 flex items-center justify-center">
+            <div className="w-full aspect-square flex flex-col items-center justify-start p-4">
+              <div className="w-full text-center">
+                <h2 className="text-2xl font-bold capitalize">{name}</h2>
+              </div>
+
+              <div className="mt-3 w-full h-full border-2 border-dashed border-slate-100 bg-white/40 p-4 space-y-3 text-lg">
+                <p>teste1</p>
+                <p>teste2</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Calendar
+export default Calendar;
